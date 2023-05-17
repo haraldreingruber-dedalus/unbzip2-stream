4fe97749b30a49e07ed24694823d05602ab7a1b3
Harald Reingruber <harald.reingruber@dedalus.com>
2023-05-17 17:13:26 +0200

    test-wasm-decompression-speed

--- test/browser/long.js
+++ test/browser/long.js
@@ -8,11 +8,18 @@ var fs = require('fs');
 test('http stream piped into unbzip2-stream results in original file content', function(t) {
     t.plan(1);
 
-    http.get({path: '/test/fixtures/vmlinux.bin.bz2', responseType: "arraybuffer"}, function(res) {
+
+    http.get({path: '/test/fixtures/sample.bz2', responseType: "arraybuffer"}, function(res) {
+        var startTime = new Date();
         res.pipe( unbzip2Stream() ).pipe(
             concat(function(data) {
-                var expected = fs.readFileSync('test/fixtures/vmlinux.bin');
-                t.equal(data.length, expected.length);
+                endTime = new Date();
+                var timeDiff = endTime - startTime; //in ms
+                // strip the ms
+                timeDiff /= 1000;
+
+                console.log('decompression took %f s', timeDiff);
+                t.equal(data.length, 214435840);
             })
         );
     });
